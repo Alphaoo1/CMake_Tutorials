f11953c8b22ba59b7040ac08e184da59d0bc18b9
Alphaoo1 <prasada31@yahoo.in>
2021-05-10 14:39:31 +0200

    update. defined add func

--- 0_CPP_trial_Project/myClasses/myClass.h
+++ 0_CPP_trial_Project/myClasses/myClass.h
@@ -6,11 +6,16 @@ class myClass
     // This is a test class
     // here we declare it's members
 
-//protected:
-//    int var1;
-//    int var2;
+protected:
+    int var3 = 2;
+    int var4 = 3;
+//    #include "class_protected_member.h"
+
 
 public:
+    int var1 = 2;
+    int var2 = 3;
+
     void printMyMSG();
 
     // Constructor
--- 0_CPP_trial_Project/myFunctions/addMyNo.h
+++ 0_CPP_trial_Project/myFunctions/addMyNo.h
@@ -1,5 +1,13 @@
 #ifndef ADDMYNO_H
 #define ADDMYNO_H
 
+int addMyNo(int a, int b)
+{
+    // This function adds two number
+    int sum;
+    sum = a + b;
+    return sum;
+}
+
 
 #endif
