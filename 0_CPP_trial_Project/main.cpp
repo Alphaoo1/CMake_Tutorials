5f8a25f784f1faf7c0fad8b28130dd879e025384
Alphaoo1 <prasada31@yahoo.in>
2021-05-10 11:39:30 +0200

    added simple message for compile

---
+++ 0_CPP_trial_Project/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream> // std::cout
+
+int main() {
+  std::cout << "Hello from Alpha001!" << std::endl;
+  std::cout << " . " << std::endl;
+  std::cout << " . " << std::endl;
+  std::cout << " . " << std::endl;
+  std::cout << "This is a trial " << std::endl;
+}
