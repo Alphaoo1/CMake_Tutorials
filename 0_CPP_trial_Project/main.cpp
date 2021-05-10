6f049a15015f6a8bcdf1907976354bd1b096aafb
Alphaoo1 <prasada31@yahoo.in>
2021-05-10 13:08:18 +0200

    using myClass

--- 0_CPP_trial_Project/main.cpp
+++ 0_CPP_trial_Project/main.cpp
@@ -1,9 +1,23 @@
 #include <iostream> // std::cout
+#include "myFunc.h"
+#include "myClass.h"
+
+#include <myClass.h>
 
 int main() {
-  std::cout << "Hello from Alpha001!" << std::endl;
-  std::cout << " . " << std::endl;
-  std::cout << " . " << std::endl;
-  std::cout << " . " << std::endl;
-  std::cout << "This is a trial " << std::endl;
+    std::cout << "Hello from Alpha001!" << std::endl;
+    std::cout << " . " << std::endl;
+    std::cout << " . " << std::endl;
+    std::cout << " . " << std::endl;
+    std::cout << "This is a trial " << std::endl;
+
+    // Include a piece of code directly
+    #include "myInclude.h"
+
+    myFunc();
+
+    myClass createObj;
+    createObj.printMyMSG();
+
+    return 0;
 }
