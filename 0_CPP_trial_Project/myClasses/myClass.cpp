912c265ded8f05e7b9de4f26808d39b43516b7f8
Alphaoo1 <prasada31@yahoo.in>
2021-05-10 12:23:07 +0200

    added definition and declaration for the class

---
+++ 0_CPP_trial_Project/myClasses/myClass.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "myClass.h"
+
+myClass::printMyMSG()
+{
+    // Here we define the member fucntion of myClass
+    std::cout << "\033[34;1m >>> myClass::printMyMSG() called \033[0m" <<  std::endl;
+}
+
+// Constructor
+myClass::myClass()
+{
+    std::cout << "\033[31;1m >>> myClass is created \033[0m" << std::endl;
+}
+
+// Destructor
+myClass::~myClass()
+{
+    std::cout << "\033[31;1m >>> myClass is destroyed \033[0m" << std::endl;
+}
---
+++ 0_CPP_trial_Project/myClasses/myClass.h
@@ -0,0 +1,23 @@
+#ifndef MYCLASS_H
+#define MYCLASS_H
+
+class myClass
+{
+    // This is a test class
+    // here we declare it's members
+
+protected:
+    int var1;
+    int var2;
+
+public:
+    void printMyMSG();
+
+    // Constructor
+    myClass();
+
+    // Destructor
+    ~myClass();
+};
+
+#endif
