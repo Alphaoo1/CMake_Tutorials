462976c71724be1e64b8f38d3fb52ed244aaa5f6
Alphaoo1 <prasada31@yahoo.in>
2021-05-10 14:40:47 +0200

    trying to declare myClass protected members

---
+++ 0_CPP_trial_Project/include/class_protected_member.h
@@ -0,0 +1,13 @@
+#ifndef CLASS_PROTECTED_MEMBER_H
+#define CLASS_PROTECTED_MEMBER_H
+
+#include <iostream> // std::cout
+#include "addMyNo.h"
+
+//std::cout << "This message comes from include directory" << std::endl;
+//std::cout << "This includes protected members to myClass" << std::endl;
+
+//int printMySum( addMyNo(var1, var2));
+//std::cout << "The sum of the addition of two variable is = " << printMySum << endl;
+
+#endif
